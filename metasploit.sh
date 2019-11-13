ae4e9e86e7e638b9912c5f0490290250526d5b0f
Krissh Kumar <57560114+krisshkumar@users.noreply.github.com>
2019-11-13 19:49:57 +0530

    Add files via upload

--- metasploit.sh
+++ metasploit.sh
@@ -30,8 +30,9 @@ cwd=$(pwd)
 msfvar=4.17.1
 msfpath='/data/data/com.termux/files/home'
 
+
 apt update
-apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
+apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make ruby libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
 
 cd $msfpath
 curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
